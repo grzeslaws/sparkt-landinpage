35aec540f0c093cdef66c7ab379ceee9a2a7ef55
grzeslaws <grzesupel@gmail.com>
2018-12-19 16:22:06 +0100

    Homa page - not copleted

---
+++ wp-content/themes/sparkt/components/bespoke-algorithms-component.php
@@ -0,0 +1,11 @@
+<div class="c-bespoke-algorithms">
+    <div class="c-bespoke-algorithms__wrapper">
+        <h1 class="c-bespoke-algorithms__headline">With the use of bespoke algorithms we can: </h1>
+        <div class="c-bespoke-algorithms__wrapper-sections">
+            <p class="c-bespoke-algorithms__section">Evaluate events such as accelerating, speeding, braking, cornering</p>
+            <p class="c-bespoke-algorithms__section">Prepare precise score of every trip and user</p>
+            <p class="c-bespoke-algorithms__section">Prepare precise score of every trip and user</p>
+        </div>
+        <div class="c-bespoke-algorithms__description">SparkT system helps insurers to optimize costs, gain competitive advantage and create new insurance products based on vehicle usage and behaviour data. </div>
+    </div>
+</div>
---
+++ wp-content/themes/sparkt/components/menage-operations-component.php
@@ -0,0 +1,15 @@
+<div class="c-menage-operations">
+    <div class="c-menage-operations__wrapper">
+        <h1 class="c-menage-operations__headline">Manage your operations more effectively with advanced telematics system for UBI purposes.</h1>
+        <div class="c-menage-operations__wrapper-content">
+            <div class="c-menage-operations__side">
+                <p class="c-menage-operations__text">SparkT is a fully-featured, modular white-label UBI platform that uses real-time sensor data from various devices to track and analyse driver behavior and measure vehicle performance.<p> 
+                <button class="o-btn">Schedule demo</button> 
+            </div>
+            <div class="c-menage-operations__side c-menage-operations__side--photos">
+                <img class="c-menage-operations__img c-menage-operations__img--1" src='<?php echo get_template_directory_uri() ?>/public/images/sparkt-console2.png' />
+                <img class="c-menage-operations__img c-menage-operations__img--2" src='<?php echo get_template_directory_uri() ?>/public/images/sparkt-console1.png' />
+            </div>
+        </div>
+    </div>
+</div>
