d2d7ac93e6f0e82e55c83608ee805c886488365e
grzeslaws <grzesupel@gmail.com>
2018-12-21 12:33:33 +0100

    Carousel

--- wp-content/themes/sparkt/assets/js/main.js
+++ wp-content/themes/sparkt/assets/js/main.js
@@ -29,6 +29,9 @@ const setMenuHeight = () => {
 };
 
 const setWidthOfProjectPhoto = () => {
+    if (!document.querySelector("[data-wrapper-text]") || !document.querySelector("[data-project]") || !document.querySelectorAll("[data-img]")) {
+        return;
+    }
     const setWidthOnImg = () => {
         const wrapperTextRight = document.querySelector("[data-wrapper-text]").getBoundingClientRect().right;
         const widthProjectEl = document.querySelector("[data-project]").getBoundingClientRect().width;
@@ -43,6 +46,80 @@ const setWidthOfProjectPhoto = () => {
     setWidthOnImg();
 };
 
+const roleSlider = () => {
+    const wrapperSliderRolesEl = document.querySelector("[wrapper-slider-roles]");
+    const wrapperRolesEl = document.querySelector("[wrapper-roles]");
+    const roleEl = wrapperSliderRolesEl.querySelector("span");
+    const roleHeight = roleEl.getBoundingClientRect().height;
+    let counter = 1;
+    let resetTranslate;
+    const roleArr = Array.from(wrapperSliderRolesEl.querySelectorAll("span"));
+
+    wrapperSliderRolesEl.style.height = `${roleHeight}px`;
+    setInterval(() => {
+        if (counter < roleArr.length - 1) {
+            clearInterval(resetTranslate);
+            wrapperRolesEl.style.transform = `translateY(${-roleHeight * counter}px)`;
+            wrapperRolesEl.style.transitionDuration = "600ms";
+            counter++;
+        } else {
+            wrapperRolesEl.style.transform = `translateY(${-roleHeight * counter}px)`;
+            resetTranslate = setInterval(() => {
+                wrapperRolesEl.style.transform = "translateY(0)";
+                wrapperRolesEl.style.transitionDuration = "0s";
+            }, 1000);
+            counter = 1;
+        }
+
+        // const firstChild = wrapperRolesEl.firstElementChild;
+        // const firstChildClone = firstChild.cloneNode(true);
+        // wrapperRolesEl.appendChild(firstChildClone);
+        // wrapperRolesEl.firstElementChild.remove();
+
+        // const resetTranslate = setInterval(() => {
+        //     wrapperRolesEl.style.transform = "translateY(0)";
+        // }, 1000);
+
+    }, 3000);
+};
+
+const carousel = () => {
+    const wrapper = document.querySelector(".c-carousel");
+    let counter = 0;
+    let wrapperWidth = wrapper.getBoundingClientRect().width;
+    window.addEventListener("resize", () => {
+        wrapperWidth = wrapper.getBoundingClientRect().width;
+        showCurrent();
+    });
+    const images = wrapper.querySelectorAll(".c-carousel__img");
+    const imagesLength = images.length;
+
+    const showCurrent = () => {
+        Array.from(images).map(img => {
+            img.style.width = `${wrapperWidth}px`;
+            img.style.transform = `translateX(${-counter * wrapperWidth}px)`;
+        });
+    };
+    showCurrent();
+
+    wrapper.querySelector(".next").addEventListener("click", () => {
+        if (counter > imagesLength - 2) {
+            return;
+        }
+        counter += 1;
+        showCurrent();
+    });
+
+    wrapper.querySelector(".prev").addEventListener("click", () => {
+        if (counter < 1) {
+            return;
+        }
+        counter -= 1;
+        showCurrent();
+    });
+};
+
+roleSlider();
 showHidePostIcon();
 setMenuHeight();
 setWidthOfProjectPhoto();
