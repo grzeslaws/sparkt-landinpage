827cc6eab796326323aae3d62910115a770dfb3f
grzeslaws <grzesupel@gmail.com>
2018-12-24 12:24:34 +0100

    About us, Contact, Modal

---
+++ wp-content/themes/sparkt/components/contact-page/contact-component.php
@@ -0,0 +1,29 @@
+<div class="c-contact">
+    <div class="c-contact__wrapper">
+        <h2 class="c-contact__description">Get in touch with us if you want to learn how our telematic system can help with your business.</h2>
+        <h1 class="c-contact__headline">Send us a message</h1>
+        <div class="c-contact__wrapper-columns">
+            <div class="c-contact__column">
+                <?php get_template_part("components/contact-form-component") ?>
+            </div>
+            <div class="c-contact__column c-contact__column--right">
+                <div class="c-contact__wrapper-address">
+                    <h3 class="c-contact__headline-smaller">SparkT</h3>
+                    <p class="c-contact__addsress">ul. Wańkowicza 5/30<br />02-796 Warsaw, Poland</p>
+                    <h3 class="c-contact__headline-smaller">Follow us</h3>
+                    <ul class="o-social__wrapper-social">
+                        <li class="o-social__social-item">
+                            <a href="https://www.facebook.com/sparkbit.software/" target="_blank" class="o-social__social-link"><img src='<?php echo get_template_directory_uri() ?>/public/images/icon-fb-black.svg' /></a>
+                        </li>
+                        <li class="o-social__social-item">
+                            <a href="https://twitter.com/_sparkbit_" target="_blank" class="o-social__social-link"><img src='<?php echo get_template_directory_uri() ?>/public/images/icon-tw-black.svg' /></a>
+                        </li>
+                        <li class="o-social__social-item">
+                            <a href="https://www.linkedin.com/company/sparkbit?trk=company_logo" target="_blank" class="o-social__social-link"><img src='<?php echo get_template_directory_uri() ?>/public/images/icon-in-black.svg' /></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ wp-content/themes/sparkt/contact-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Contact
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php get_template_part("components/contact-page/contact-component") ?>
+
+<?php get_footer(); ?>
